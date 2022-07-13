949b2088bb3f1d9b4777da325074e0620d3aed74
Venkata Prasad Pinapathruni <p.venkataprasad@outlook.com>
2022-07-13 12:21:15 +0530

    Code AppAmigo

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public DataContext _dataContext { get; }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetAll()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetOne(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/Migrations/20220713061415_NewInitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class NewInitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUser",
+                table: "AppUser");
+
+            migrationBuilder.RenameTable(
+                name: "AppUser",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "AppUser");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUser",
+                table: "AppUser",
+                column: "UserId");
+        }
+    }
+}
